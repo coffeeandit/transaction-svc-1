f39148b3b1961f7cf4bd1c18fe4622a7fc4c7584
Cléber da Silveira <csilveir@gmail.com>
2020-01-09 14:07:43 -0300

    build

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 sed -i "s/VERSION_APP/$(cat gradle.properties | grep "$version" | cut -d'=' -f2)/g" build.yml
 oc project workshop
 oc replace -f build.yml
-oc start-build transaction-build
+oc start-build transaction-svc-build
 git checkout build.yml
