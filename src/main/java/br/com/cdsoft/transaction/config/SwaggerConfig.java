a316be86a33f1e1be6e5af0c7f56b9bccf98c003
Cléber da Silveira <csilveir@gmail.com>
2020-01-09 17:05:51 -0300

    Swagger

--- src/main/java/br/com/cdsoft/transaction/config/SwaggerConfig.java
+++ src/main/java/br/com/cdsoft/transaction/config/SwaggerConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
@@ -15,6 +17,10 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(new ApiInfo("Workshop CoffeeAndIT", "API ", "1.0.1", "",
+                        new Contact("coffeeandit", "http://www.coffeeandit.com.br", "coffeeandit@coffeeandit.com.br")
+                        , "Apache", "", null
+                ))
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
